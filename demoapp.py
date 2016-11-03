d9a7336563e937abbc8303425ded5ba28d0335a0
Ryan Criss <rcriss@cisco.com>
2016-11-03 14:45:11 -0500

    The Lovable Winners

--- demoapp.py
+++ demoapp.py
@@ -13,10 +13,10 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/hello/world')
 
-if __name__ == '__main__':
-    # Runn Flask
-    app.run(debug=True, host='0.0.0.0', port=int("5000"))
-    # pass
+class HelloUniverse(Resource):
+    def get(self):
+        text = "Hello Universe!"
+        return text
 
 api.add_resource(HelloUniverse, '/hello/universe')
 
